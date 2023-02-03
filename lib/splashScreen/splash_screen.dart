d4ce17dd72a5d334e74b55666427971ebdaa4b43
atozas <atozasindia@gmail.com>
2023-02-03 16:08:27 +0530

    First Commit/ Registration and login

--- lib/splashScreen/splash_screen.dart
+++ lib/splashScreen/splash_screen.dart
@@ -15,7 +15,7 @@ class MySplashScreen extends StatefulWidget {
 
 class _MySplashScreenState extends State<MySplashScreen> {
   startTimer() {
-    Timer(const Duration(seconds: 3), () async {
+    Timer( const Duration(seconds: 3), () async {
       if (await fAuth.currentUser != null) {
         currentFirebaseUser = fAuth.currentUser;
         //send user to the main screen
